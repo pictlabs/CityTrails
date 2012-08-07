5319653ded91c00fe1bd679e2cf2855b6fc6ab37
Richard Geier <richard@pictlabs.com>
2012-08-07 01:01:56 -0400

    Adding Region Database Information

--- CityTrails/CTAppDelegate.m
+++ CityTrails/CTAppDelegate.m
@@ -146,15 +146,15 @@
     }
     
     NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"CityTrails"];
-    /*if (![[NSFileManager defaultManager] fileExistsAtPath:[storeURL path]]) {
-        NSURL *preloadURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"CityTrails" ofType:@"sqlite"]];
+    if (![[NSFileManager defaultManager] fileExistsAtPath:[storeURL path]]) {
+        NSURL *preloadURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"SQLiteImporter" ofType:@"sqlite"]];
         NSError* err = nil;
         
         if (![[NSFileManager defaultManager] copyItemAtURL:preloadURL toURL:storeURL error:&err]) {
-            NSLog(@"Oops, could copy preloaded data");
+            NSLog(@"Oops, couldn't copy preloaded data");
         }
-    }*/
-    
+    }
+       
     NSError *error = nil;
     _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
     if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
